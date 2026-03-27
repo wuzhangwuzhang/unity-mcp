06f5bcb7be71594b5df9996b75767891c58d40a2
wuzhang <wuzhang@cxxmobi.com>
2026-03-27 11:02:09 +0800

    "com.ivanmurzak.unity.mcp": "0.57.3" 切换到"coplay-mcp",注释unity-mcp的插件代码

--- Assets/Editor/Promat_Custom.cs
+++ Assets/Editor/Promat_Custom.cs
@@ -1,20 +1,20 @@
-﻿using System.ComponentModel;
-using com.IvanMurzak.McpPlugin;
-using com.IvanMurzak.McpPlugin.Common.Model;
+﻿// using System.ComponentModel;
+// using com.IvanMurzak.McpPlugin;
+// using com.IvanMurzak.McpPlugin.Common.Model;
 
-namespace com.IvanMurzak.Unity.MCP.Editor.API
-{
-    public class Promat_Custom
-    {
-        [McpPluginPromptType]
-        public static class Prompt_ScriptingCode
-        {
-            [McpPluginPrompt(Name = "add-event-system", Role = Role.User)]
-            [Description("Implement UnityEvent-based communication system between GameObjects.")]
-            public static  string AddEventSystem()
-            {
-                return "Create event system using UnityEvents, UnityActions, or custom event delegates for decoupled communication between game systems and components.";
-            }
-        }
-    }
-}
+// namespace com.IvanMurzak.Unity.MCP.Editor.API
+// {
+//     public class Promat_Custom
+//     {
+//         [McpPluginPromptType]
+//         public static class Prompt_ScriptingCode
+//         {
+//             [McpPluginPrompt(Name = "add-event-system", Role = Role.User)]
+//             [Description("Implement UnityEvent-based communication system between GameObjects.")]
+//             public static  string AddEventSystem()
+//             {
+//                 return "Create event system using UnityEvents, UnityActions, or custom event delegates for decoupled communication between game systems and components.";
+//             }
+//         }
+//     }
+// }
